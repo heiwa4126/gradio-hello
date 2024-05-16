4c061ac271ed39676c5271407a1de50b33a16a07
heiwa4126 <heiwa4126@gmail.com>
2024-05-16 09:35:07 +0900

    update

--- src/gradio_hello/main.py
+++ src/gradio_hello/main.py
@@ -1,18 +1,16 @@
 import gradio as gr
 
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
-
-
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
-)
-
-
 def main():
+    def greet(name, intensity):
+        return "Hello, " + name + "!" * int(intensity)
+
+    demo = gr.Interface(
+        fn=greet,
+        inputs=["text", "slider"],
+        outputs=["text"],
+    )
+
     demo.launch()
     # demo.launch(server_port=8080)
 
